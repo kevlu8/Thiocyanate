f84e4afdb32dc6337fc52a41aeb897c4ade11ee9
Kevin Lu <kevie.lu8@gmail.com>
2022-10-14 12:42:00 -0600

    Add basic crashing capabilities

--- Thiocyanate/include.hpp
+++ Thiocyanate/include.hpp
@@ -1,3 +1,12 @@
 #pragma once
 #include <Windows.h>
 #include <math.h>
+#include <vector>
+
+using std::vector;
+
+typedef struct PAYLOADPARAMS {
+	HDC hdc;
+	int x;
+	int y;
+} PAYLOADPARAMS;
--- Thiocyanate/payloads.hpp
+++ Thiocyanate/payloads.hpp
@@ -1,6 +1,17 @@
 #pragma once
 #include "include.hpp"
 
-void p1(HDC hdc) {
-	
+#define fori100 for (int i=0;i<100;i++)
+#define forj100 for (int j=0;j<100;j++)
+#define fori(x) for (int i=0;i<x;i++)
+#define forj(x) for (int j=0;j<x;j++)
+#define random(x) rand() % x
+#define randomRGB random(255)
+
+void p1(PAYLOADPARAMS p) {
+	fori(p.x) {
+		forj(p.y) {
+			SetPixel(p.hdc, i, j, RGB(randomRGB, randomRGB, randomRGB));
+		}
+	}
 }
